ec9c48c470d995f6c589d9089fc761600d3304ae
rafal a <rafalandrzejczuk1@gmial.com>
2022-11-14 15:38:56 +0100

    zmiana

---
+++ js/main.js
@@ -0,0 +1,13 @@
+
+
+let myheding = document.querySelector("h1");
+myheding.textContent = 'Skrypt działa prawidłowo'
+/* WAZNE SA NAWIASI itp */
+myheding.onclick = function() {
+  alert('Skrypt dziala prawidłowo')
+}
+console.log(myheding)
+
+
+
+ 
